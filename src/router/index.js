83d2b5c6495965b86d2a5a1a2fd95025cab1cbb1
FMiklos23 <fuleki.miklos.2021i@bankitatabanya.hu>
2025-10-16 21:03:45 +0200

    Tippek aloldal elkészítése

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainLayout from '../layouts/MainLayout.vue'
 import QuizLayout from '../layouts/QuizLayout.vue'
+import TipsLayout from '../layouts/TipsLayout.vue'
 
 const routes = [
   { path: '/', name: 'home', component: MainLayout },
   { path: '/quiz', name: 'quiz', component: QuizLayout },
+  { path: '/tips', name: 'tips', component: TipsLayout }
 
 ]
 
